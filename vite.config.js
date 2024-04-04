2dea091d98ca3feee4d94741d8f8abd63f2a8a17
sssk1led <sssk1led@gmail.com>
2024-04-04 20:53:30 +0300

    Создание скелета проэкта 1

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    host: true,
+    port: 3000,    
+    proxy: {
+      '/get': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      },
+      '/post': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
+  }
+})
